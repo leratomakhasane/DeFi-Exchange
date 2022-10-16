8ed105ba4278b67bd3eb82988e57c2e20dff5b0d
Lerato Rampai <87371242+leratomakhasane@users.noreply.github.com>
2022-10-16 16:37:45 +0200

    landing page

---
+++ defi-app/utils/addLiquidity.js
@@ -0,0 +1,48 @@
+import {Contract, utils} from "ethers";
+import{
+    EXCHANGE_CONTRACT_ABI,
+    EXCHANGE_CONTRACT_ADDRESS,
+    TOKEN_CONTRACT_ABI,
+    TOKEN_CONTRACT_ADDRESS
+} from "../constants";
+
+export const addLiquidity = async (signer, addCDAmountWei, addEtherAmountWei) => {
+    try {
+      // create a new instance of the token contract
+      const tokenContract = new Contract(
+        TOKEN_CONTRACT_ADDRESS,
+        TOKEN_CONTRACT_ABI,
+        signer
+      );
+      // create a new instance of the exchange contract
+      const exchangeContract = new Contract(
+        EXCHANGE_CONTRACT_ADDRESS,
+        EXCHANGE_CONTRACT_ABI,
+        signer
+      );
+      // Because CD tokens are an ERC20, user would need to give the contract allowance
+      // to take the required number CD tokens out of his contract
+      let tx = await tokenContract.approve(
+        EXCHANGE_CONTRACT_ADDRESS,
+        addCDAmountWei.toString()
+      );
+      await tx.wait();
+      // After the contract has the approval, add the ether and cd tokens in the liquidity
+      tx = await exchangeContract.addLiquidity(addCDAmountWei, {
+        value: addEtherAmountWei,
+      });
+      await tx.wait();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  export const calculateCD = async (_addEther = "0", etherBalanceContract, cdTokenReserve) => {
+    // convert to a Bignumber before we do calculations using the `parseEther` function from `ethers.js`
+    const _addEtherAmountWei = utils.parseEther(_addEther);
+    
+    const cryptoDevTokenAmount = _addEtherAmountWei
+      .mul(cdTokenReserve)
+      .div(etherBalanceContract);
+    return cryptoDevTokenAmount;
+  };
